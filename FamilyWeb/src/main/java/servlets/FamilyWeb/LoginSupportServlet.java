36fd8d2661db3c5030a621552810c2a66d90c651
michielio <michiel.wilm@gmail.com>
2016-03-10 22:25:22 +0100

    Refactor 2 LoginSupportServlet
    De volgende refactor methoden zijn toegepast op deze class:
    - Rename (regel 20)
    - Introduce parameter (regel 42 en toegepast op regel 58)
    - Introduce paramter object (MessageInfo class op regel 134)

--- FamilyWeb/src/main/java/servlets/FamilyWeb/LoginSupportServlet.java
+++ FamilyWeb/src/main/java/servlets/FamilyWeb/LoginSupportServlet.java
@@ -16,20 +16,17 @@ import domain.FamilyWeb.User;
 public class LoginSupportServlet extends HttpServlet {
 
 	private final String PAGE_LOGIN = "/login.jsp";
-	private final String PAGE_FORGOT_USERNAME = "/forgot_username.jsp";
-	private final String PAGE_FORGOT_PASSWORD = "/forgot_password.jsp";
-	
-	private HttpServletRequest req = null;
-	private String option = "";
 
-	@Override
+    private HttpServletRequest httpReqField = null;
+
+    @Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-		this.req = req;
+		this.httpReqField = req;
 		DatabaseInterface db = (DatabaseInterface) this.getServletContext().getAttribute("dbController");
 
 		// get the option that is called this clould be forgotPassword, forgotUsername
-		option = (req.getParameter("option") != null) ? req.getParameter("option") : "";
+        String option = (req.getParameter("option") != null) ? req.getParameter("option") : "";
 
 		// If forgot password called.
 		if (option.equals("forgotPassword")) {
@@ -37,17 +34,18 @@ public class LoginSupportServlet extends HttpServlet {
 		}
 		// If forgot username called.
 		else if (option.equals("forgotUsername")) {
-			this.ForgotUsername(req, resp, db);
+			this.ForgotUsername(req, resp, db, "FamilyWeb Gebruikersnaam vergeten");
 		}
 
 	}
 
-	private void ForgotUsername(HttpServletRequest req, HttpServletResponse resp, DatabaseInterface db) throws ServletException, IOException {
+	private void ForgotUsername(HttpServletRequest req, HttpServletResponse resp, DatabaseInterface db, String mailSubjects) throws ServletException, IOException {
 		String message = "";
 		User user = null;
 		String email = req.getParameter("email");
 
-		if (email != null && !email.trim().equals("")) {
+        String PAGE_FORGOT_USERNAME = "/forgot_username.jsp";
+        if (email != null && !email.trim().equals("")) {
             for (User u : db.getAllUsers()) {
                 if (u.getEmail().equals(email.trim())) {
                     user = u;
@@ -57,24 +55,24 @@ public class LoginSupportServlet extends HttpServlet {
 
             if (user != null) {
                 // Send username mail to the user.
-                String mailSubject = "FamilyWeb Gebruikersnaam vergeten";
+                String mailSubject = mailSubjects;
                 String mailMessage = "<div class='text'><p>Beste <span class='bold_text'>" + user.getForename() + "</span>,</p><p>Er is een gebruikersnaam vergeten aangevraagd.</p></div><div class='information'><table class='custom_table'><tr class='row'><td class='data'>Gebruikersnaam</td><td class='data'>" + user.getUsername() + "</td></tr></table></div><div class='text'><p>Mochten er zich problemen voordoen met het inloggen of met het gebruik van de applicatie dan kunt u contact opnemen met de <a href='mailto:info@familyweb.nl'>administrator.</a></p><p>Wij hopen dat u een fijne ervaring heeft met de applicatie.</p><p>FamilyWeb</p></div>";
                 MailService mailService = new MailService(user, mailSubject, mailMessage);
                 message += (mailService.sendMail()) ? message : message + " Mailservice fout de mail is niet verzonden, Raadpleeg de administrator.";
 
                 if (message.equals("")) {
-                    this.setMessage("success", "De gebruikersnaam is verzonden naar je mail.");
+                    this.setMessage(new MessageInfo("success", "De gebruikersnaam is verzonden naar je mail."));
                     req.getRequestDispatcher(PAGE_LOGIN).forward(req, resp);
                 } else {
-                    this.setMessage("error", message);
+                    this.setMessage(new MessageInfo("error", message));
                     req.getRequestDispatcher(PAGE_FORGOT_USERNAME).forward(req, resp);
                 }
             } else {
-                this.setMessage("error", "Email niet correct of niet gevonden in ons systeem.");
+                this.setMessage(new MessageInfo("error", "Email niet correct of niet gevonden in ons systeem."));
                 req.getRequestDispatcher(PAGE_FORGOT_USERNAME).forward(req, resp);
             }
         } else {
-            this.setMessage("error", "Email niet correct of niet gevonden in ons systeem.");
+            this.setMessage(new MessageInfo("error", "Email niet correct of niet gevonden in ons systeem."));
             req.getRequestDispatcher(PAGE_FORGOT_USERNAME).forward(req, resp);
         }
 	}
@@ -84,14 +82,15 @@ public class LoginSupportServlet extends HttpServlet {
 		User user = null;
 		String username = req.getParameter("username").trim();
 
-		if (username != null && !username.equals("")) {
+        String PAGE_FORGOT_PASSWORD = "/forgot_password.jsp";
+            if (username != null && !username.equals("")) {
 
-            for (User u : db.getAllUsers()) {
-                if (u.getUsername().equals(username.trim())) {
-                    user = u;
-                    break;
+                for (User u : db.getAllUsers()) {
+                     if (u.getUsername().equals(username.trim())) {
+                        user = u;
+                        break;
+                     }
                 }
-            }
 
             if (user != null) {
 
@@ -111,30 +110,29 @@ public class LoginSupportServlet extends HttpServlet {
 
                 if (message.equals("")) {
                     user.updateDB();
-                    this.setMessage("success", "Nieuw wachtwoord is verzonden naar je mail.");
+                    this.setMessage(new MessageInfo("success", "Nieuw wachtwoord is verzonden naar je mail."));
                     req.getRequestDispatcher(PAGE_LOGIN).forward(req, resp);
                 } else {
-                    this.setMessage("error", message);
+                    this.setMessage(new MessageInfo("error", message));
                     req.getRequestDispatcher(PAGE_FORGOT_PASSWORD).forward(req, resp);
                 }
 
             } else {
-                this.setMessage("error", "Gebruikersnaam niet correct of niet gevonden in ons systeem.");
+                this.setMessage(new MessageInfo("error", "Gebruikersnaam niet correct of niet gevonden in ons systeem."));
                 req.getRequestDispatcher(PAGE_FORGOT_PASSWORD).forward(req, resp);
             }
         } else {
-            this.setMessage("error", "Gebruikersnaam niet correct of niet gevonden in ons systeem.");
+            this.setMessage(new MessageInfo("error", "Gebruikersnaam niet correct of niet gevonden in ons systeem."));
             req.getRequestDispatcher(PAGE_FORGOT_PASSWORD).forward(req, resp);
         }
 	}
 
 	/**
 	 * Method to set information message on page.
-	 * @param messageType String type of message could be success, error or warning
-	 * @param message String the message to display
-	 */
-	private void setMessage(String messageType, String message) {
-		req.setAttribute("messageType", messageType);
-		req.setAttribute("message", message);
+     * @param messageInfo
+     */
+	private void setMessage(MessageInfo messageInfo) {
+		httpReqField.setAttribute("messageType", messageInfo.getMessageType());
+		httpReqField.setAttribute("message", messageInfo.getMessage());
 	}
 }
